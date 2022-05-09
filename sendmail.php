bcee9c108e837ac022a80a33aaec69d2b153fe20
Habjul <96185378+Habjul@users.noreply.github.com>
2022-05-10 00:10:08 +0300

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,71 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('habjul30@gmail.com', 'Юлія Кузьменко');
+	//Кому отправить
+	$mail->addAddress('derga123der@gmail.com');
+	//Тема письма
+	$mail->Subject = 'I love you';
+
+	//Рука
+	$hand = "Правая";
+	if($_POST['hand'] == "left"){
+		$hand = "Левая";
+	}
+
+	//Тело письма
+	$body = '<h1>Встречайте супер письмо!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Імя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['hand']))){
+		$body.='<p><strong>Рука:</strong> '.$hand.'</p>';
+	}
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Вік:</strong> '.$_POST['age'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Повідомлення:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото прикріплено</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Помилка';
+	} else {
+		$message = 'Дані відправлені!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
